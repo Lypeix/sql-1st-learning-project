e8f0beccee7c6d88946a2c1b3f6bd8eef78b6d23
Lypeix <patrykkudla1010@gmail.com>
2026-07-14 22:48:42 +0200

    Update database.py

--- database.py
+++ database.py
@@ -4,42 +4,42 @@ def connect():
     return sqlite3.connect("notes.db")
 
 def create_table():
-    connection = connect()
-    cursor = connection.cursor()
+    connection = connect() # opens db
+    cursor = connection.cursor() # creates object that sends commands to sql
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS notes (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    title TEXT NOT NULL,                  
-                   content TEXT NOT NULL,
-                   created_at TEXT NOT NULL
+    CREATE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        created_at TEXT NOT NULL
         )
-    """
-    )
+    """) # cursor.execute makes that so the text is read by SQLite which then parses the valid vocabualry 
 
-    connection.commit()
+    connection.commit() # saves changes
     connection.close()
 
-def add_note(title, content, created_at):
+def add_note(title, content, created_at): # adds these 3 values from create_table to notes.db
     connection = connect()
     cursor = connection.cursor()
 
     cursor.execute("""
         INSERT INTO notes (title, content, created_at)
         VALUES (?, ?, ?)
-    """, (title, content, created_at))
+                """, (title, content, created_at))
 
     connection.commit()
     connection.close()
+
 
 def get_notes():
     connection = connect()
     cursor = connection.cursor()
 
     cursor.execute("SELECT id, title, created_at FROM notes")
-    notes = cursor.fetchall()
+    notes = cursor.fetchall # grabs selected rows and returns them as python objects
 
-    connection.close()
+    connection.close
 
     return notes
 
