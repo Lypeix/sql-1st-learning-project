5a334ea199bc05344014b3af1bbb7eeabbc1e97f
Lypeix <patrykkudla1010@gmail.com>
2026-07-15 10:54:01 +0200

    Update _main.py

--- _main.py
+++ _main.py
@@ -1,4 +1,15 @@
-from database import get_notes_by_id
+from database import create_table, add_note, get_notes, get_notes_by_id
+
+create_table()
+
+notes = get_notes()
+
+if len(notes) == 0:
+    add_note("Pizza note", "That pizza from yesterday had too much cheese on it", "15-07-2026")
+    notes = get_notes()
+
+for note in notes:
+    print(f"{note[0]} - {note[1]} - {note[2]}")
 
 note_id = int(input("Choose note ID: "))
 note = get_notes_by_id(note_id)
