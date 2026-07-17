86569a2a3e8920629b7db55aa2b585cedeed0cc2
Patryk <patrykkudla1010@gmail.com>
2026-07-17 14:01:03 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,10 +12,10 @@ def get_text(prompt):
 def get_int(prompt):
     while True:
         try:
-            return int(input(prompt))
+            return int(input(prompt)) # asks for input, converts it to integer and returns it at the same time!
 
         except ValueError:
             print("Invalid number")
 
 def get_timestamp():
-    return datetime.now().strftime("%d-%m-%Y | %H:%M")
+    return datetime.now().strftime("%d-%m-%Y | %H:%M") # get scurrent date and time, then formats it into readable string
