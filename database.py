6ee3a0c2a1f78fe3bb6a98c22e2b4ee1fdd404a9
Patryk <patrykkudla1010@gmail.com>
2026-07-17 13:46:10 +0200

    Update database.py

--- database.py
+++ database.py
@@ -39,7 +39,7 @@ def get_notes():
     cursor = connection.cursor()
 
     cursor.execute("SELECT id, title, created_at FROM notes")
-    notes = cursor.fetchall() # grabs selected rows and returns them as python objects
+    notes = cursor.fetchall() # grabs all the rows and returns them as python objects
 
     connection.close()
 
